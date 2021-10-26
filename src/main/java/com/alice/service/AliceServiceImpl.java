3cf974647db26bdce868b823f344233ff5072236
Opachi <egorodinczov@yandex.ru>
2021-10-26 19:34:19 +0500

    init

--- src/main/java/com/alice/service/AliceServiceImpl.java
+++ src/main/java/com/alice/service/AliceServiceImpl.java
@@ -14,11 +14,19 @@ public class AliceServiceImpl implements AliceService {
         AliceResponseDto aliceResponseDto = new AliceResponseDto();
         ResponseDto responseDto = new ResponseDto();
         String text = aliceRequestDto.getRequest().getOriginal_utterance();
-        text = text.replaceAll(" ","");
-        if(text.matches("[0-9]+") && text.length()==10) {
+        Range range = null;
+        if (text.contains("номер")) {
+            range = getValueStartAndEnd(text, "номер");
+        }
+        if (text.contains("почту")) {
+            range = getValueStartAndEnd(text, "почту");
+        }
+        text = text.substring(range.end, range.start);
+        text = text.replaceAll(" ", "");
+        if (text.matches("[0-9]+") && text.length() == 10) {
             responseDto.setText(text);
-            responseDto.setTts(text.substring(0,3) + " " + text.substring(3,6) + " " + text.substring(6,8) + " " + text.substring(8,10));
-        } else if(EmailValidator.getInstance().isValid(text)) {
+            responseDto.setTts(text.substring(0, 3) + " " + text.substring(3, 6) + " " + text.substring(6, 8) + " " + text.substring(8, 10));
+        } else if (EmailValidator.getInstance().isValid(text)) {
             responseDto.setText(text);
             responseDto.setTts(text);
         } else {
@@ -27,6 +35,26 @@ public class AliceServiceImpl implements AliceService {
         }
         aliceResponseDto.setResponse(responseDto);
         return aliceResponseDto;
+    }
+
+    public Range getValueStartAndEnd(String text, String word) {
+        Range range = new Range();
+        range.start = text.indexOf(word) + 6;
+        for (int i = range.start; i < text.length(); i++) {
+            if (text.charAt(i) >= 1072 && text.charAt(i) <= 1105) {
+                range.end = i;
+                break;
+            }
+        }
+        if (range.end == 0) {
+            range.end = text.length();
+        }
+        return range;
+    }
+
+    class Range {
+        private int start;
+        private int end;
     }
 
 }
